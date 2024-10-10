a6f77a357cc57c56240b19113f83527aff086b4f
Geronimo001 <szgym29@gmail.com>
2024-10-10 17:04:09 +0200

    Styling commit 4

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -4,6 +4,9 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
@@ -27,6 +30,7 @@ public class App extends Application {
         clip.setArcWidth(40); // Rounding corners
         clip.setArcHeight(40);
         root.setClip(clip);
+
 
         stage.initStyle(StageStyle.TRANSPARENT);
 
