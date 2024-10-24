fbbc563b7218503d85d80c5d4c660d9e57dd686d
Geronimo001 <szgym29@gmail.com>
2024-10-24 12:39:53 +0200

    Doctor Page imported

---
+++ src/main/java/org/example/DoctorController.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class DoctorController {
+    @FXML
+    private Label allatFaj;
+
+    @FXML
+    private Label allatSzam;
+
+    @FXML
+    private Label emailCim;
+
+    @FXML
+    private Label gazdaNev;
+
+    @FXML
+    private Label kertVizsgalat;
+
+    @FXML
+    private Label megjegyzes;
+
+    @FXML
+    private Label telefonszam;
+}
